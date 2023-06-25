6909f2068c5cce5525741d6912de3b648df30408
David Arnold <80532381+optimisedu@users.noreply.github.com>
2023-06-25 13:41:37 +0100

    Create syload.py
    TODO CREATE HONEYPOT

---
+++ syload.py
@@ -0,0 +1,55 @@
+import time
+
+def boot_system():
+    print("Starting system boot...")
+    time.sleep(0.3)
+
+    print("Analyzing processor speed...")
+    time.sleep(0.3)
+
+    print("Checking memory modules...")
+    time.sleep(0.3)
+
+    print("Loading system resources...")
+    time.sleep(0.3)
+
+    print("Initializing peripherals...")
+    time.sleep(0.3)
+
+    print("Establishing network connection...")
+    time.sleep(0.3)
+
+    print("Booting operating system...")
+    time.sleep(0.3)
+
+    print("Configuring system settings...")
+    time.sleep(0.3)
+
+    print("Performing system diagnostics...")
+    time.sleep(0.3)
+
+    print("Running startup scripts...")
+    time.sleep(0.3)
+
+    print("Checking disk drives...")
+    time.sleep(0.3)
+
+    print("Initializing graphics subsystem...")
+    time.sleep(0.3)
+
+    print("Loading user profiles...")
+    time.sleep(0.3)
+
+    print("Mounting file systems...")
+    time.sleep(0.3)
+
+    print("Starting system services...")
+    time.sleep(0.3)
+
+    print("Launching user interface...")
+    time.sleep(0.3)
+
+    print("System boot complete!")
+
+# Call the function to start the system boot
+boot_system()
